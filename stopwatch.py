be1328f43589395afdf50b9a3d2a97376e460dc0
EarnestM2002 <mcdaniele@lopers.unk.edu>
2024-09-25 16:43:33 +0000

    Stopwatch Final

--- stopwatch.py
+++ stopwatch.py
@@ -4,13 +4,29 @@ import datetime
 # Get time for start
 input("Press Enter to start.")
 start_time = str(datetime.datetime.now().time()).split(":")
-start_hour = start_time[0]
-start_minute = start_time[1]
-start_second = start_time[2]
+start_hour = int(start_time[0])
+start_minute = int(start_time[1])
+start_second = float(start_time[2])
+
 
 # Get time for stop
 input("Press Enter again to stop.")
 stop_time = str(datetime.datetime.now().time()).split(":")
-stop_hour = stop_time[0]
-stop_minute = stop_time[1]
-stop_second = stop_time[2]
+stop_hour = int(stop_time[0])
+stop_minute = int(stop_time[1])
+stop_second = float(stop_time[2])
+
+difference = float(stop_second) - float(start_second)
+print(difference)
+
+difference = int(stop_minute) - int(start_minute)
+print(difference)
+
+difference = int(stop_hour) - int(start_hour)
+print(difference)
+
+# Adjust if the seconds or minutes go negative
+if stop_second < 0:
+    stop_second += 60
+    stop_minute -= 1
+
